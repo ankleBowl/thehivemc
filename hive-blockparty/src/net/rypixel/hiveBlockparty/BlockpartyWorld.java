46bc0fa62408b3bede9dc5d2e3cc645a9f71d79f
ankleBowl <specialreeree6@gmail.com>
2021-03-28 08:14:53 -0700

    Removed speed particles + lowered effect level

--- hive-blockparty/src/net/rypixel/hiveBlockparty/BlockpartyWorld.java
+++ hive-blockparty/src/net/rypixel/hiveBlockparty/BlockpartyWorld.java
@@ -189,7 +189,7 @@ public class BlockpartyWorld {
 		hp.serverId = id;
 		hp.mcPlayer.setGameMode(GameMode.ADVENTURE);
 		hp.mcPlayer.removePotionEffect(PotionEffectType.INVISIBILITY);
-		hp.mcPlayer.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999, 1, false, true));
+		hp.mcPlayer.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999, 0, false, false));
 		hp.mcPlayer.setAllowFlight(false);
 		
 		hp.mcPlayer.setFoodLevel(20);
